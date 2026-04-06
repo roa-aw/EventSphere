834f6fa6cb3d121e08957cc7e630fdfd28ebb11c
roa-aw <rouahawata@gmail.com>
2026-04-07 00:33:23 +0300

    gitignore

--- obj/Debug/net10.0/EventSphere.API.AssemblyInfo.cs
+++ obj/Debug/net10.0/EventSphere.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventSphere.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a07d0ebcea5e82ab8f319a280d803bcfe8467892")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventSphere.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventSphere.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
